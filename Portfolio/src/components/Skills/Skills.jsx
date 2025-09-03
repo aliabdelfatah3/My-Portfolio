3ad3d4ebf2740f4a6bbac50f711a3e27db26ed7a
Ali Abdelfattah Saad <133794591+aliabdelfatah3@users.noreply.github.com>
2025-09-03 03:43:16 +0300

    edit skill again

--- Portfolio/src/components/Skills/Skills.jsx
+++ Portfolio/src/components/Skills/Skills.jsx
@@ -17,15 +17,15 @@ import Webflow_logo from "./Pic/Webflow_logo.png";
 const logos = [
   { src: HTML_logo, alt: "HTML" },
   { src: CSS_logo, alt: "CSS" },
-  { src: React_logo, alt: "React" },
   { src: JavaScript_logo, alt: "JavaScript" },
+  { src: React_logo, alt: "React" },
+  { src: Redux_logo, alt: "Redux" },
+  { src: Tailwindcss_logo, alt: "Tailwind CSS" },
+  { src: TypeScript_logo, alt: "TypeScript" },
   { src: Git_logo, alt: "Git" },
   { src: GitHub_logo, alt: "GitHub" },
   { src: Bootstrap_logo, alt: "Bootstrap" },
   { src: Material_UI_logo, alt: "Material UI" },
-  { src: Redux_logo, alt: "Redux" },
-  { src: Tailwindcss_logo, alt: "Tailwind CSS" },
-  { src: TypeScript_logo, alt: "TypeScript" },
   { src: Vercel_logo, alt: "Vercel" },
   { src: Vite_logo, alt: "Vite" },
   { src: Webflow_logo, alt: "Webflow" },
@@ -33,18 +33,18 @@ const logos = [
 
 function Skills() {
   return (
-    <div className="w-full h-full flex justify-center items-center px-4">
-      <div className="flex flex-col items-center justify-center bg-Bej dark:bg-purple-950 shadow-2xl rounded-lg p-6 sm:p-8 w-full sm:w-[90%] md:w-[80%] lg:w-[70%]">
-        <h1 className="text-2xl md:text-3xl font-bold dark:text-white mb-4">
+    <div className="flex-1 flex h-full -translate-y-[90px] items-center justify-center px-4">
+      <div className="flex flex-col items-center justify-center bg-Bej dark:bg-purple-950 shadow-2xl rounded-lg p-6 sm:p-8 w-full max-w-[1200px]">
+        <h1 className="text-2xl md:text-3xl xl:text-4xl font-bold dark:text-white mb-6 text-center">
           Skills
         </h1>
-        <div className="flex flex-wrap justify-center gap-4 sm:gap-6 md:gap-10">
+        <div className="flex flex-wrap justify-center gap-4 sm:gap-6 md:gap-8 lg:gap-10">
           {logos.map((logo, index) => (
             <img
               key={index}
               src={logo.src}
               alt={logo.alt}
-              className="size-16 sm:size-20 md:size-24 lg:hover:scale-110 transition-transform duration-300"
+              className="w-10 h-10 sm:w-12 sm:h-12 md:w-16 md:h-16 lg:w-20 lg:h-20 xl:w-24 xl:h-24 hover:scale-110 transition-transform duration-300"
             />
           ))}
         </div>
